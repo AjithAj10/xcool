8d48da255848b0283d00ca369c1df05de21b274e
AjithAj10 <104498680+AjithAj10@users.noreply.github.com>
2023-05-06 16:18:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,74 +6,62 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Category from "./Category";
 
-interface cardData {
-  id: any;
-  name: String;
-  teacher: String | null;
-  category_id: Number;
-  type: any;
-  description: String;
-  created_at: String;
-  updated_at: String;
-  prerequisites: String | null;
-  parentId: Number | null;
-}
 
 function App() {
-  const [data, setData] = useState<{ data: cardData[] }>({
-    data: [],
-  });
+  const [data, setData] = useState<any>();
   useEffect(() => {
     let config = {
       method: "get",
       maxBodyLength: Infinity,
       url: "https://xcool.in/api/test5m23",
       headers: {
-        'token': '1|FWItRXH5DCAN9rjBjIhfH9KMnprvKZweoK2Jfi5T', 
-        'Authorization': 'Bearer 1|FWItRXH5DCAN9rjBjIhfH9KMnprvKZweoK2Jfi5T', 
+        token: "1|FWItRXH5DCAN9rjBjIhfH9KMnprvKZweoK2Jfi5T",
+        Authorization: "Bearer 1|FWItRXH5DCAN9rjBjIhfH9KMnprvKZweoK2Jfi5T",
       },
     };
 
     axios
       .request(config)
       .then((response) => {
-        setData(response);
-        console.log(JSON.stringify(response.data));
+        setData(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
+
   return (
     <Box>
-      {data.data
-        ?.filter(
-          (music: { type: string; parentId: any }) =>
-            music.type === "Category" && !music.parentId
-        )
-        .map((musicCard: { id?: React.Key | null | undefined; name?: any }) => {
-          return (
-            <span key={musicCard?.id}>
-              <Card
-                sx={{
-                  margin: "1em",
-                  width: "50%",
-                  borderRadius: "10px",
-                  boxShadow: "rgba(0, 0, 0, 0.64) 0px 3px 8px",
-                  backgroundColor: "rgb(43, 34, 69)",
-                  color: "white",
-                }}
-              >
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    {musicCard?.name}
-                  </Typography>
-                </CardContent>
-              </Card>
-              <Category data={data} musicCard={musicCard} />
-            </span>
-          );
-        })}
+      {data ?
+        data.data &&
+        data.data.filter(
+            (music: { type: string; parentId: any }) =>
+              music.type === "Category" && !music.parentId
+          )
+          .map((musicCard: { id?: any; name?: any }) => {
+            return (
+              <span key={musicCard?.id}>
+                <Card
+                  sx={{
+                    margin: "1em",
+                    width: "50%",
+                    borderRadius: "10px",
+                    boxShadow: "rgba(0, 0, 0, 0.64) 0px 3px 8px",
+                    backgroundColor: "rgb(43, 34, 69)",
+                    color: "white",
+                  }}
+                >
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      {musicCard?.name}
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Category data={data} musicCard={musicCard} />
+              </span>
+            );
+          }) : <Typography variant="h4">Loading...</Typography>}
     </Box>
   );
 }
